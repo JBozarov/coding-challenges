f6cb630c9dfacb146df6c054ff057d58887e434a
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2021-04-25 12:47:22 -0500

    Solved ReverseInteger

--- java/src/com/javacodingchallenges/groups/leetcode/ReverseInteger.java
+++ java/src/com/javacodingchallenges/groups/leetcode/ReverseInteger.java
@@ -3,19 +3,21 @@ package com.javacodingchallenges.groups.leetcode;
 public class ReverseInteger {
 
     public static void main(String[] args) {
-        int i = reverse(1534236469);
-        System.out.println(i);
+        int k = 1563847412;
+        int i = reverse(k);
+        System.out.println("i is " + i);
     }
 
-
     public static int reverse(int x) {
-        int result = 0;
-        if (x > 0) {
-            result = Integer.parseInt(new StringBuilder(String.valueOf(x)).reverse().toString());
+        long result = 0;
+        if (x < 0) {
+            result = -Long.parseLong(new StringBuilder(String.valueOf((int) Math.sqrt(Math.pow(x, 2)))).reverse().toString());
         } else {
-            int temp = x * -1;
-            result = Integer.parseInt(new StringBuilder(String.valueOf(temp)).reverse().toString());
+            result = Long.parseLong(new StringBuilder(String.valueOf(x)).reverse().toString());
         }
-        return x >= 0 ? result : -result;
+        if (result > Math.pow(2, 31) - 1 || result < -Math.pow(2, 31)) {
+            return 0;
+        }
+        return (int) result;
     }
 }
