1cd53e795ced57dd768b7f2cb4d6116e9eed6e8c
jbozarov <tech@jbozarov.com>
2021-06-08 15:52:15 -0400

    greeting

---
+++ src/io/javabrains/GreetingImpl.java
@@ -0,0 +1,8 @@
+package io.javabrains;
+
+public class GreetingImpl implements Greeting{
+    @Override
+    public void perform() {
+        System.out.println("hello this is implementation ");
+    }
+}
--- src/io/javabrains/Main.java
+++ src/io/javabrains/Main.java
@@ -1,10 +1,13 @@
 package io.javabrains;
 
-public class HelloWorldGreeting {
+public class Main {
 
     public static void main(String[] args) {
         Greeting lambdaGreeting = () -> System.out.println("hello");
-
         lambdaGreeting.perform();
+
+        GreetingImpl greeting = new GreetingImpl();
+        greeting.perform();
+
     }
 }
